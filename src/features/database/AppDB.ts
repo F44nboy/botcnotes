fcb30be3e8003ac247473dfcf49c31b967cfb175
Milan Berlik <milanberlik@gmail.com>
2025-11-24 21:42:09 +0100

    updated database to include script type and script table

--- src/features/database/AppDB.ts
+++ src/features/database/AppDB.ts
@@ -1,16 +1,18 @@
 // src/db/AppDB.ts
 import Dexie, { type Table } from "dexie"
-import type { Player } from "@/database/types/player"
+import type { Player } from "@/features/database/types/player"
+import type { Script } from "@/features/database/types/script"
 
 export default class AppDB extends Dexie {
   players!: Table<Player, number>
+  scripts!: Table<Script, string>
 
   constructor() {
     super("BloodyNotesDB")
 
-    this.version(1).stores({
-      players: "id++, seat, name"
+    this.version(2).stores({
+      players: "id++, seat, name",
+      scripts: "&id" // 'id' ist der Primärschlüssel (der Skriptname)
     })
   }
 }
-
---
+++ src/features/database/types/script.ts
@@ -0,0 +1,5 @@
+// src/features/database/types/script.ts
+export type Script = {
+  id: string; // Der Name des Skripts, z.B. "trouble_brewing"
+  data: never;  // Das vollständige JSON-Objekt des Skripts
+}
